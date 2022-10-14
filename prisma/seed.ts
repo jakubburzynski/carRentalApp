e1d038d5dbe2c6f3e658ecbffbe4060898156625
jakubburzynski <kubatkh@gmail.com>
2022-10-14 21:52:58 +0200

    feat: create prisma seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.unitType.createMany({
+        data: [
+            { name: "Metric" },
+            { name: "Imperial" }
+        ]
+    });
+
+    await prisma.fuelType.createMany({
+        data: [
+            { name: "Gas" },
+            { name: "Diesel" },
+            { name: "Electric" },
+            { name: "Hybrid" },
+        ],
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
