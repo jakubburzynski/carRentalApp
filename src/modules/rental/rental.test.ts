dbcad11d2c528597931c42b4475253994f4357c5
jakubburzynski <kubatkh@gmail.com>
2022-10-17 19:14:20 +0200

    test: adjust 'POST /api/v1/rental' tests

--- src/modules/rental/rental.test.ts
+++ src/modules/rental/rental.test.ts
@@ -1,18 +1,30 @@
-import { test, describe, expect, beforeAll, afterAll } from "@jest/globals";
+import {
+    test,
+    describe,
+    expect,
+    beforeAll,
+    afterAll,
+    afterEach,
+} from "@jest/globals";
 import { faker } from "@faker-js/faker";
 
 import createFastifyServer from "../../loaders/fastify";
+import uuidRegex from "../../utils/uuidRegex.util";
 
 describe("POST /api/v1/rental", () => {
     let app: Awaited<ReturnType<typeof createFastifyServer>>;
     beforeAll(async () => {
         app = await createFastifyServer();
+        await app.prisma.rental.deleteMany();
+    });
+    afterEach(async () => {
+        await app.prisma.rental.deleteMany();
     });
 
     test("should create a rental", async () => {
         const payload = {
             name: faker.company.name(),
-            unitTypeId: 0,
+            unitTypeId: 1,
         };
         const response = await app.inject({
             method: "POST",
@@ -21,30 +33,45 @@ describe("POST /api/v1/rental", () => {
         });
 
         expect(response.statusCode).toBe(201);
-        expect(response.json()).toBe({
-            uuid: expect.any(String),
+        expect(response.json()).toEqual({
+            uuid: expect.stringMatching(uuidRegex),
             name: payload.name,
         });
     });
 
     test("should not create more than one rental", async () => {
-        const payload = {
+        const firstPayload = {
             name: faker.company.name(),
-            unitTypeId: 0,
+            unitTypeId: 1,
         };
-        const response = await app.inject({
+        const firstResponse = await app.inject({
             method: "POST",
             url: "/api/v1/rental",
-            payload,
+            payload: firstPayload,
         });
 
-        expect(response.statusCode).toBe(409);
-        expect(response.json().message).toEqual(
+        const secondPayload = {
+            name: faker.company.name(),
+            unitTypeId: 2,
+        };
+        const secondResponse = await app.inject({
+            method: "POST",
+            url: "/api/v1/rental",
+            payload: secondPayload,
+        });
+
+        expect(firstResponse.statusCode).toBe(201);
+        expect(firstResponse.json()).toEqual({
+            uuid: expect.stringMatching(uuidRegex),
+            name: firstPayload.name,
+        });
+        expect(secondResponse.statusCode).toBe(409);
+        expect(secondResponse.json().message).toEqual(
             "It is not possible to create more than one rental",
         );
     });
 
-    test("should check for invalid unit type id", async () => {
+    test("should not create rental with not existing unit type id", async () => {
         const payload = {
             name: faker.company.name(),
             unitTypeId: 1867,
@@ -55,13 +82,13 @@ describe("POST /api/v1/rental", () => {
             payload,
         });
 
-        expect(response.statusCode).toBe(422);
+        expect(response.statusCode).toBe(409);
         expect(response.json().message).toEqual("Invalid unit type id");
     });
 
     test("should check for missing name", async () => {
         const payload = {
-            unitTypeId: 0,
+            unitTypeId: 1,
         };
         const response = await app.inject({
             method: "POST",
@@ -70,10 +97,28 @@ describe("POST /api/v1/rental", () => {
         });
 
         const responseJson = response.json();
-        expect(response.statusCode).toBe(422);
-        expect(responseJson.message).toEqual("Validation error");
-        expect(responseJson.errors).toHaveProperty("name");
-        expect(responseJson.errors.name).toContain("name is required");
+        expect(response.statusCode).toBe(400);
+        expect(responseJson.message).toEqual(
+            "body must have required property 'name'",
+        );
+    });
+
+    test("should check for name shorter than 3 char", async () => {
+        const payload = {
+            name: "ab",
+            unitTypeId: 1,
+        };
+        const response = await app.inject({
+            method: "POST",
+            url: "/api/v1/rental",
+            payload,
+        });
+
+        const responseJson = response.json();
+        expect(response.statusCode).toBe(400);
+        expect(responseJson.message).toEqual(
+            "body/name must NOT have fewer than 3 characters",
+        );
     });
 
     test("should check for missing unitTypeId", async () => {
@@ -87,10 +132,26 @@ describe("POST /api/v1/rental", () => {
         });
 
         const responseJson = response.json();
-        expect(response.statusCode).toBe(422);
-        expect(responseJson.message).toEqual("Validation error");
-        expect(responseJson.errors).toHaveProperty("unitTypeId");
-        expect(responseJson.errors.name).toContain("unitTypeId is required");
+        expect(response.statusCode).toBe(400);
+        expect(responseJson.message).toEqual(
+            "body must have required property 'unitTypeId'",
+        );
+    });
+
+    test("should check for type of unitTypeId other than number", async () => {
+        const payload = {
+            name: faker.company.name(),
+            unitTypeId: "metric",
+        };
+        const response = await app.inject({
+            method: "POST",
+            url: "/api/v1/rental",
+            payload,
+        });
+
+        const responseJson = response.json();
+        expect(response.statusCode).toBe(400);
+        expect(responseJson.message).toEqual("body/unitTypeId must be number");
     });
 
     afterAll(async () => {
