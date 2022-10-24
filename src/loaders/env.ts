ddab44f99305c36400451f8d830954249c807eb0
jakubburzynski <kubatkh@gmail.com>
2022-10-24 16:03:03 +0200

    refactor: manage env manually instead of using @fastify/env

--- src/loaders/env.ts
+++ src/loaders/env.ts
@@ -1,11 +1,40 @@
+import fp from "fastify-plugin";
 import { Static, Type } from "@sinclair/typebox";
+import { envSchema } from "env-schema";
+import { FastifyPluginAsync } from "fastify";
 
-const envSchema = Type.Object({
+const initialEnvSchema = Type.Object({
     DATABASE_URL: Type.String(),
-    PORT: Type.Number({ default: 3000 }),
-    HOST: Type.String({ default: "0.0.0.0" }),
+    SENDGRID_API_KEY: Type.String(),
+    SENDGRID_SENDER_ADDRESS: Type.String(),
+    SERVER_PORT: Type.Number({ default: 3000 }),
+    SERVER_HOST: Type.String({ default: "localhost" }),
+    SERVER_HTTPS: Type.Boolean({ default: false }),
+});
+type InitialEnvSchema = Static<typeof initialEnvSchema>;
+const initialEnvConfig = envSchema<InitialEnvSchema>({
+    schema: initialEnvSchema,
+    dotenv: true,
 });
 
-export type EnvSchema = Static<typeof envSchema>;
+function buildBaseUrl(): string {
+    const protocol = initialEnvConfig.SERVER_HTTPS ? "https" : "http";
+    const port =
+        initialEnvConfig.SERVER_PORT === 80 ||
+        initialEnvConfig.SERVER_PORT === 443
+            ? ""
+            : `:${initialEnvConfig.SERVER_PORT}`;
+    return `${protocol}://${initialEnvConfig.SERVER_HOST}${port}`;
+}
 
-export default envSchema;
+export type EnvConfig = InitialEnvSchema & { SERVER_BASE_URL: string };
+const envConfig: EnvConfig = {
+    ...initialEnvConfig,
+    SERVER_BASE_URL: buildBaseUrl(),
+};
+
+const envPlugin: FastifyPluginAsync = async (server) => {
+    server.decorate("config", envConfig);
+};
+
+export default fp(envPlugin);
--- src/loaders/fastify.ts
+++ src/loaders/fastify.ts
@@ -1,17 +1,16 @@
 import fastify, { FastifyServerOptions } from "fastify";
-import fastifyEnv from "@fastify/env";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import { PrismaClient } from "@prisma/client";
 
-import envSchema, { EnvSchema } from "./env";
+import envPlugin, { EnvConfig } from "./env";
 import rentalRoutes from "../modules/rental/rental.route";
 import prismaPlugin from "./prisma";
 import unitTypeRoutes from "../modules/unitType/unitType.route";
 import rentalManagerRoutes from "../modules/rentalManager/rentalManager.route";
 
 declare module "fastify" {
-    interface FastifyInstance {
-        config: EnvSchema;
+    export interface FastifyInstance {
+        config: EnvConfig;
         prisma: PrismaClient;
     }
 }
@@ -27,11 +26,7 @@ export default async function createFastifyServer(
             },
         },
     }).withTypeProvider<TypeBoxTypeProvider>();
-    await server.register(fastifyEnv, {
-        schema: envSchema,
-        dotenv: true,
-        data: process.env,
-    });
+    await server.register(envPlugin);
     await server.register(prismaPlugin);
 
     server.register(rentalRoutes, { prefix: "/api/v1/rentals" });
