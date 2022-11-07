64e9687cbe3aebd6d424a21d24ac8440cad2321b
jakubburzynski <kubatkh@gmail.com>
2022-11-07 15:21:55 +0100

    feat: create 'putActivateRentalManager' validation schemas

--- src/modules/rentalManager/rentalManager.schema.ts
+++ src/modules/rentalManager/rentalManager.schema.ts
@@ -5,7 +5,7 @@ import passwordRegex from "../../utils/passwordRegex.util";
 export const postRegisterRentalManagerBody = Type.Object({
     name: Type.String({ minLength: 3 }),
     email: Type.String({ format: "email" }),
-    password: Type.RegEx(passwordRegex, {}),
+    password: Type.RegEx(passwordRegex),
     rentalUuid: Type.String({ format: "uuid" }),
 });
 
@@ -21,4 +21,32 @@ export const postRegisterRentalManagerResponse = Type.Object({
 
 export type PostRegisterRentalManagerResponse = Static<
     typeof postRegisterRentalManagerResponse
+>;
+
+export const putActivateRentalManagerBody = Type.Object({
+    active: Type.Literal(true),
+});
+
+export const putActivateRentalManagerParams = Type.Object({
+    uuid: Type.String({ format: "uuid" }),
+});
+
+export const putActivateRentalManagerQuery = Type.Object({
+    token: Type.String({ minLength: 32, maxLength: 32 }),
+});
+
+export type PutActivateRentalManagerBody = Static<
+    typeof putActivateRentalManagerBody
+>;
+export type PutActivateRentalManagerParams = Static<
+    typeof putActivateRentalManagerParams
+>;
+export type PutActivateRentalManagerQuery = Static<
+    typeof putActivateRentalManagerQuery
+>;
+
+export const putActivateRentalManagerResponse = Type.Null();
+
+export type PutActivateRentalManagerResponse = Static<
+    typeof putActivateRentalManagerResponse
 >;
