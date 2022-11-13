cf8f39a7fa014ea5db8dc4873aaca1058a4bb21a
jakubburzynski <kubatkh@gmail.com>
2022-11-13 23:51:04 +0100

    feat: create 'postCreateVehicle' schemas

---
+++ src/modules/vehicle/vehicle.schema.ts
@@ -0,0 +1,25 @@
+import { Static, Type } from "@sinclair/typebox";
+
+export const postCreateVehicleBody = Type.Object({
+    brand: Type.String({ minLength: 2 }),
+    model: Type.String({ minLength: 1 }),
+    year: Type.Number({ minimum: 1900, maximum: new Date().getFullYear() + 1 }),
+    description: Type.Optional(Type.String({ minLength: 3 })),
+    mileage: Type.Number({ minimum: 1 }),
+    licensePlate: Type.String({ minLength: 1, maxLength: 20 }),
+    pricePerDay: Type.Number({ minimum: 1 }),
+    name: Type.Optional(Type.String({ minLength: 3 })),
+    rentalUuid: Type.String({ format: "uuid" }),
+    fuelTypeUuid: Type.String({ format: "uuid" }),
+});
+
+export type PostCreateVehicleBody = Static<typeof postCreateVehicleBody>;
+
+export const postCreateVehicleResponse = Type.Object({
+    uuid: Type.String({ format: "uuid" }),
+    name: Type.String(),
+});
+
+export type PostCreateVehicleResponse = Static<
+    typeof postCreateVehicleResponse
+>;
