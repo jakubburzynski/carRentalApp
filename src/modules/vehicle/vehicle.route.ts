e935828eaa7b9cbcf3bb70feec527f177a385b3c
jakubburzynski <kubatkh@gmail.com>
2022-11-23 00:22:47 +0100

    feat: create 'postUploadVehiclePhoto' controller

--- src/modules/vehicle/vehicle.route.ts
+++ src/modules/vehicle/vehicle.route.ts
@@ -1,12 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { Prisma } from "@prisma/client";
+import { MultipartFile } from "@fastify/multipart";
+import bytes from "bytes";
 
 import {
     PostCreateVehicleBody,
     postCreateVehicleBody,
     postCreateVehicleResponse,
+    postUploadVehiclePhotoParams,
+    PostUploadVehiclePhotoParams,
+    postUploadVehiclePhotoResponse,
 } from "./vehicle.schema";
 import { createVehicle } from "./vehicle.service";
+import { uploadVehiclePhoto } from "../vehiclePhoto/vehiclePhoto.service";
+import isFastifyError from "../../utils/isFastifyError.util";
 
 export default async function vehicleRoutes(server: FastifyInstance) {
     server.post<{ Body: PostCreateVehicleBody }>(
@@ -34,6 +41,66 @@ export default async function vehicleRoutes(server: FastifyInstance) {
                 fuelTypeUuid: request.body.fuelTypeUuid,
             });
             return reply.status(201).send(vehicle);
+        },
+    );
+
+    server.post<{
+        Params: PostUploadVehiclePhotoParams;
+    }>(
+        "/:uuid/photos",
+        {
+            preHandler: server.auth([server.isLoggedIn]),
+            schema: {
+                params: postUploadVehiclePhotoParams,
+                response: {
+                    201: postUploadVehiclePhotoResponse,
+                },
+            },
+        },
+        async (request, reply) => {
+            if (!request.isMultipart()) {
+                return reply.status(415).send({
+                    message: "Request content type is not multipart",
+                });
+            }
+
+            let photo: MultipartFile | undefined;
+            try {
+                photo = await request.file({
+                    limits: {
+                        fields: 0,
+                        files: 1,
+                        fileSize: bytes("2MB"),
+                    },
+                });
+            } catch (err) {
+                if (isFastifyError(err) && err.code === "FST_FIELDS_LIMIT") {
+                    return reply.status(400).send({
+                        message: "Only allowed field type is photo",
+                    });
+                }
+
+                throw err;
+            }
+
+            if (!photo) {
+                return reply.status(400).send({
+                    message: "No vehicle photo uploaded",
+                });
+            }
+
+            if (photo.fieldname !== "photo") {
+                return reply.status(422).send({
+                    message:
+                        "Vehicle photo should be attached to the 'photo' field",
+                });
+            }
+
+            const vehiclePhoto = await uploadVehiclePhoto(
+                request.params.uuid,
+                photo,
+            );
+            return reply.status(201).send(vehiclePhoto);
         },
     );
 }
