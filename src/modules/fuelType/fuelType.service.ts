75d03cae19796ed73f60192e37d907d4bda6d424
jakubburzynski <kubatkh@gmail.com>
2023-01-04 12:30:03 +0100

    feat: create 'findAllFuelTypes' service

--- src/modules/fuelType/fuelType.service.ts
+++ src/modules/fuelType/fuelType.service.ts
@@ -1,5 +1,9 @@
 import { prisma } from "../../loaders/prisma";
 
+export async function findAllFuelTypes() {
+    return prisma.fuelType.findMany();
+}
+
 export async function findFuelTypeByUuid(uuid: string) {
     return prisma.fuelType.findUnique({
         where: { uuid },
